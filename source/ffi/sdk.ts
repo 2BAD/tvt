efe3c6ef2d330cf62ceff35d6d91798302034b72
Jason Hyde <github@2bad.me>
2024-05-07 18:11:19 +0200

    [ts] slightly better naming

--- source/ffi/sdk.ts
+++ source/ffi/sdk.ts
@@ -6,7 +6,7 @@ import { NET_SDK_IPC_DEVICE_INFO } from './struct/NET_SDK_IPC_DEVICE_INFO.ts'
 import { type LOG_LEVEL } from './types.ts'
 
 const path = resolve('bin/linux/libdvrnetsdk.so')
-const dvr = koffi.load(path)
+const lib = koffi.load(path)
 
 // koffi.alias('DWORD', 'uint32_t')
 // koffi.alias('WORD', 'uint16_t')
@@ -30,9 +30,9 @@ type SDK = {
   // BOOL NET_SDK_Cleanup();
   cleanup: () => boolean
   // BOOL NET_SDK_SetConnectTime(DWORD dwWaitTime = 5000, DWORD dwTryTimes = 3);
-  connect: (waitTime: number, retryTimes: number) => boolean
+  setConnectTimeout: (waitTime: number, retryTimes: number) => boolean
   // BOOL NET_SDK_SetReconnect(DWORD dwInterval = 5000, BOOL bEnableRecon = TRUE);
-  reconnect: (interval: number, enableRecon: boolean) => boolean
+  setReconnectInterval: (interval: number, enableRecon: boolean) => boolean
   // LONG NET_SDK_Login(char *sDVRIP,WORD wDVRPort,char *sUserName,char *sPassword, LPNET_SDK_DEVICEINFO lpDeviceInfo);
   login: (ip: string, port: number, username: string, password: string, deviceInfo: Record<string, unknown>) => number
   // BOOL NET_SDK_GetDeviceInfo(LONG lUserID, LPNET_SDK_DEVICEINFO pdecviceInfo);
@@ -50,34 +50,34 @@ type SDK = {
 }
 
 export const sdk: SDK = {
-  getSDKVersion: dvr.func('NET_SDK_GetSDKVersion', 'uint32_t', []),
-  getSDKBuildVersion: dvr.func('NET_SDK_GetSDKBuildVersion', 'uint32_t', []),
-  discoverDevice: dvr.func('NET_SDK_DiscoverDevice', 'int', [
+  getSDKVersion: lib.func('NET_SDK_GetSDKVersion', 'uint32_t', []),
+  getSDKBuildVersion: lib.func('NET_SDK_GetSDKBuildVersion', 'uint32_t', []),
+  discoverDevice: lib.func('NET_SDK_DiscoverDevice', 'int', [
     koffi.out(koffi.pointer(NET_SDK_DEVICE_DISCOVERY_INFO)),
     'int',
     'int'
   ]),
-  getDeviceIPCInfo: dvr.func('NET_SDK_GetDeviceIPCInfo', 'bool', [
+  getDeviceIPCInfo: lib.func('NET_SDK_GetDeviceIPCInfo', 'bool', [
     'long',
     koffi.out(koffi.pointer(NET_SDK_IPC_DEVICE_INFO)),
     'long',
     'long'
   ]),
-  init: dvr.func('NET_SDK_Init', 'bool', []),
-  cleanup: dvr.func('NET_SDK_Cleanup', 'bool', []),
-  connect: dvr.func('NET_SDK_SetConnectTime', 'bool', ['uint32_t', 'uint32_t']),
-  reconnect: dvr.func('NET_SDK_SetReconnect', 'bool', ['uint32_t', 'bool']),
-  login: dvr.func('NET_SDK_Login', 'long', [
+  init: lib.func('NET_SDK_Init', 'bool', []),
+  cleanup: lib.func('NET_SDK_Cleanup', 'bool', []),
+  setConnectTimeout: lib.func('NET_SDK_SetConnectTime', 'bool', ['uint32_t', 'uint32_t']),
+  setReconnectInterval: lib.func('NET_SDK_SetReconnect', 'bool', ['uint32_t', 'bool']),
+  login: lib.func('NET_SDK_Login', 'long', [
     'string',
     'uint16_t',
     'string',
     'string',
     koffi.out(koffi.pointer(LPNET_SDK_DEVICEINFO))
   ]),
-  getDeviceInfo: dvr.func('NET_SDK_GetDeviceInfo', 'bool', ['long', koffi.out(koffi.pointer(LPNET_SDK_DEVICEINFO))]),
-  setupAlarmChanel: dvr.func('NET_SDK_SetupAlarmChan', 'long', ['long']),
-  closeAlarmChanel: dvr.func('NET_SDK_CloseAlarmChan', 'bool', ['long']),
-  triggerAlarm: dvr.func('NET_SDK_SetDeviceManualAlarm', 'bool', ['long', 'long', 'long', 'long', 'bool']),
-  getLastError: dvr.func('NET_SDK_GetLastError', 'uint32_t', []),
-  setLogToFile: dvr.func('NET_SDK_SetLogToFile', 'bool', ['bool', 'string', 'bool', 'int'])
+  getDeviceInfo: lib.func('NET_SDK_GetDeviceInfo', 'bool', ['long', koffi.out(koffi.pointer(LPNET_SDK_DEVICEINFO))]),
+  setupAlarmChanel: lib.func('NET_SDK_SetupAlarmChan', 'long', ['long']),
+  closeAlarmChanel: lib.func('NET_SDK_CloseAlarmChan', 'bool', ['long']),
+  triggerAlarm: lib.func('NET_SDK_SetDeviceManualAlarm', 'bool', ['long', 'long', 'long', 'long', 'bool']),
+  getLastError: lib.func('NET_SDK_GetLastError', 'uint32_t', []),
+  setLogToFile: lib.func('NET_SDK_SetLogToFile', 'bool', ['bool', 'string', 'bool', 'int'])
 }
