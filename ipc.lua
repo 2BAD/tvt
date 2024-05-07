1d4e3c66f5cb2553d1b69a84f4424ca113db1163
Jason Hyde <github@2bad.me>
2024-05-07 03:24:26 +0200

    [lua] update disector with opcodes extracted from header files

--- ipc.lua
+++ ipc.lua
@@ -12,10 +12,43 @@ local vs_packet_type = {
   [22] = "22"
 }
 
-local vs_cmd = {
-  [1] = "Question",
-  [2] = "Answer",
-  [21] = "Init"
+local vs_cmd_type = {
+  [0x0100] = "CMD_BASE_LOGIN",
+  [0x0101] = "CMD_REQUEST_LOGIN_DECODER",
+  [0x0102] = "CMD_REQUEST_LOGOUT_DECODER",
+  [0x0103] = "CMD_END_NUM_LOGIN",
+
+  [0x0300] = "CMD_BASE_ENCODER",
+  [0x0301] = "CMD_STREAM_START",
+  [0x0302] = "CMD_STREAM_SWITCH",
+  [0x0303] = "CMD_STREAM_STOP",
+  [0x0304] = "CMD_KEYFRAME",
+  [0x0305] = "CMD_END_NUM_ENCODER",
+
+  [0x010000] = "CMD_BASE_NUM_CFG",
+  [0x010001] = "CMD_CONFIG_GET",
+  [0x010002] = "CMD_CONFIG_SET",
+
+  [0x020000] = "CMD_BASE_NUM_CMS_CONTROL_ENC",
+  [0x020001] = "CMD_SWITCH_ENC",
+  [0x020002] = "CMD_STOP_DECODE",
+  [0x020003] = "CMD_START_DECODE",
+
+  [0x030000] = "CMD_BASE_NUM_CTRL",
+  [0x030001] = "CMD_MSG_LOGIN",
+  [0x030002] = "CMD_MSG_LOGOUT",
+  [0x030003] = "CMD_MSG_REBOOT",
+  [0x030004] = "CMD_MSG_SHUTDOWN",
+  [0x030005] = "CMD_MSG_EXFACTORY_SETUP",
+  [0x030006] = "CMD_CHANGE_TIME",
+  [0x030007] = "CMD_Exception_Set",
+  [0x030008] = "CMD_GET_VERSION_INFO",
+  [0x030009] = "MSG_GET_DEVICE_INFO",
+  [0x030010] = "CMD_NET_DEVICE_SEARCH",
+  [0x030011] = "CMD_GET_ALL_USERINFO",
+  [0x030012] = "CMD_SET_ALL_USERINFO",
+  [0x030013] = "CMD_UPDATE_START",
+  [0x030014] = "CMD_UPDATE_DATA"
 }
 
 local vs_direction = {
@@ -25,11 +58,14 @@ local vs_direction = {
 
 -- Define the fields
 local fields = {
-    topic = ProtoField.string("ipc.topic", "Topic"),
-    frame_length = ProtoField.uint32("ipc.len", "Frame Length"),
-    cmd = ProtoField.uint8("ipc.cmd", "Command", base.DEC, vs_cmd),
-    type = ProtoField.uint8("ipc.type", "Type", base.DEC, vs_packet_type),
+    head = ProtoField.string("ipc.head", "Head"),
+    data_length = ProtoField.uint32("ipc.len", "Data Length"),
+    cmd = ProtoField.uint32("ipc.cmd", "Command", base.HEX),
+    cmdType = ProtoField.uint32("ipc.cmdType", "Type", base.HEX, vs_cmd_type, 0x0000FFFF),
+    cmdId = ProtoField.uint8("ipc.cmdId", "Command ID", base.DEC),
+    cmdVer = ProtoField.uint8("ipc.cmdVer", "Command Version", base.DEC),
     counter = ProtoField.uint32("ipc.counter", "Counter"),
+    sdkVersion = ProtoField.uint32("ipc.sdkVersion", "SDK Version"),
     session = ProtoField.uint32("ipc.session", "Session"),
     direction = ProtoField.uint8("ipc.direction", "Direction", base.DEC, vs_direction),
     login = ProtoField.bytes("ipc.login", "Login", base.SPACE),
@@ -49,92 +85,94 @@ function protocol.dissector(buffer, pinfo, tree)
     pinfo.cols.protocol = protocol.name
 
     -- Create t_ipc subtree
-    local t_ipc = tree:add(protocol, buffer(), "IPC Data")
+    local t_ipc = tree:add(protocol, buffer(), "IPC")
     local offset = 0
 
-    -- Add fields to the t_ipc
-    t_ipc:add(fields.topic, buffer(offset, 4))
-    local p_topic = buffer(offset, 4):string()
+    -- Create t_header subtree
+    local t_header = t_ipc:add(protocol, buffer(offset), "Header")
+
+    -- Add fields to the t_header
+    t_header:add(fields.head, buffer(offset, 4))
+    local p_head = buffer(offset, 4):string()
     offset = offset + 4
 
-    t_ipc:add_le(fields.frame_length, buffer(offset, 4))
+    t_header:add_le(fields.data_length, buffer(offset, 4))
     offset = offset + 4
 
-    if p_topic == '1111' and buffer:len() == offset then
+    if p_head == '1111' and buffer:len() == offset then
       pinfo.cols.info = 'keepalive'
     end
 
     if buffer:len() > offset then
 
-      -- Create t_header subtree
-      local t_header = t_ipc:add(protocol, buffer(offset), "Header")
+      -- Create t_command subtree
+      local t_command = t_ipc:add(protocol, buffer(offset), "Command")
 
-      t_header:add(fields.cmd, buffer(offset, 1))
-      local p_cmd = buffer(offset, 1):uint()
-      offset = offset + 1
-
-      t_header:add(fields.type, buffer(offset, 1))
-      local p_type = buffer(offset, 1):uint()
-      offset = offset + 1
-
-      t_header:add(fields.unk8, buffer(offset, 1))
-      offset = offset + 1
-
-      t_header:add(fields.direction, buffer(offset, 1))
-      local p_direction = buffer(offset, 1):uint()
-      offset = offset + 1
-
-      pinfo.cols.info = vs_direction[p_direction] .. "(" .. vs_cmd[p_cmd] .. " " .. vs_packet_type[p_type] .. ")"
-
-      t_header:add_le(fields.counter, buffer(offset, 4))
+      t_command:add_le(fields.cmd, buffer(offset, 4))
+      local p_cmd = buffer(offset, 4):uint()
+      t_command:add_le(fields.cmdType, buffer(offset, 4))
+      local p_cmdType = buffer(offset, 4):uint()
       offset = offset + 4
+
+      t_command:add_le(fields.cmdId, buffer(offset, 4))
+      local p_cmdId = buffer(offset, 4):uint()
+      offset = offset + 4
+
+      t_command:add_le(fields.cmdVer, buffer(offset, 4))
+      local p_cmdVer = buffer(offset, 4):uint()
+      offset = offset + 4
+
+      t_command:add_le(fields.data_length, buffer(offset, 4))
+      offset = offset + 4
+
+      -- pinfo.cols.info = vs_direction[p_direction] .. "(" .. vs_cmd[p_cmd] .. " " .. vs_packet_type[p_type] .. ")"
 
       -- Init Session
       if p_cmd == 21 and p_type == 6 then
-        t_header:add(fields.unk8, buffer(offset, 1))
+        t_command:add(fields.unk8, buffer(offset, 1))
         offset = offset + 1
-        t_header:add(fields.unk8, buffer(offset, 1))
+        t_command:add(fields.unk8, buffer(offset, 1))
         offset = offset + 1
-        t_header:add(fields.unk8, buffer(offset, 1))
+        t_command:add(fields.unk8, buffer(offset, 1))
         offset = offset + 1
-        t_header:add(fields.unk8, buffer(offset, 1))
+        t_command:add(fields.unk8, buffer(offset, 1))
         offset = offset + 1
-        t_header:add_le(fields.unk32, buffer(offset, 4))
+        t_command:add_le(fields.unk32, buffer(offset, 4))
         offset = offset + 4
-        t_header:add_le(fields.unk32, buffer(offset, 4))
+        t_command:add_le(fields.unk32, buffer(offset, 4))
         offset = offset + 4
-        t_header:add_le(fields.session, buffer(offset, 4))
+        t_command:add_le(fields.session, buffer(offset, 4))
         offset = offset + 4
-        t_header:add_le(fields.unk32, buffer(offset, 4))
+        t_command:add_le(fields.unk32, buffer(offset, 4))
         offset = offset + 4
-        t_header:add_le(fields.unk32, buffer(offset, 4))
+        t_command:add_le(fields.unk32, buffer(offset, 4))
         offset = offset + 4
-        t_header:add_le(fields.unk32, buffer(offset, 4))
+        t_command:add_le(fields.unk32, buffer(offset, 4))
         offset = offset + 4
 
-        t_header:add(fields.unk8, buffer(offset, 1))
+        t_command:add(fields.unk8, buffer(offset, 1))
         offset = offset + 1
-        t_header:add(fields.unk8, buffer(offset, 1))
+        t_command:add(fields.unk8, buffer(offset, 1))
         offset = offset + 1
-        t_header:add(fields.unk8, buffer(offset, 1))
+        t_command:add(fields.unk8, buffer(offset, 1))
         offset = offset + 1
-        t_header:add(fields.unk8, buffer(offset, 1))
+        t_command:add(fields.unk8, buffer(offset, 1))
         offset = offset + 1
 
-        t_header:add_le(fields.unk32, buffer(offset, 4))
+        t_command:add_le(fields.unk32, buffer(offset, 4))
         offset = offset + 4
-        t_header:add_le(fields.unk32, buffer(offset, 4))
+        t_command:add_le(fields.unk32, buffer(offset, 4))
         offset = offset + 4
-        t_header:add_le(fields.unk32, buffer(offset, 4))
+        t_command:add_le(fields.unk32, buffer(offset, 4))
         offset = offset + 4
-        t_header:add_le(fields.unk32, buffer(offset, 4))
+        t_command:add_le(fields.unk32, buffer(offset, 4))
         offset = offset + 4
       else
-        t_header:add_le(fields.unk32, buffer(offset, 4))
+        t_command:add_le(fields.unk32, buffer(offset, 4))
         offset = offset + 4
-        t_header:add_le(fields.frame_length, buffer(offset, 4))
+        t_command:add_le(fields.data_length, buffer(offset, 4))
         offset = offset + 4
-        local t_frame = t_header:add(protocol, buffer(offset), "Frame")
+        local t_frame = t_command:add(protocol, buffer(offset), "Frame")
 
         if buffer:len() > offset then
           if p_type == 1 and p_direction == 0 then
