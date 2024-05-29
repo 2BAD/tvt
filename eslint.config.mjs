02d4ba997b22d32d573bff79750271697b783b2a
Jason Hyde <github@2bad.me>
2024-05-29 03:51:00 +0200

    [conf] switch to flat config for eslint-config-love

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,7 @@
-import { FlatCompat } from '@eslint/eslintrc'
+import love from 'eslint-config-love'
 import jsdoc from 'eslint-plugin-jsdoc'
 import prettier from 'eslint-plugin-prettier/recommended'
 import vitest from 'eslint-plugin-vitest'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-// mimic CommonJS variables
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
-
-const compat = new FlatCompat({
-  baseDirectory: dirname
-})
 
 export default [
   // ignores key should be used first and without any other keys in the configuration object
@@ -19,14 +9,14 @@ export default [
     ignores: ['build', 'docs']
   },
   jsdoc.configs['flat/recommended-typescript-error'],
-  // mimic ESLintRC-style extends
-  ...compat.extends('eslint-config-love'),
+  love,
   prettier,
   // general
   {
     rules: {
       'import/no-default-export': 'error',
-      'import/no-unresolved': 'error',
+      // don't work without typescript import specifier support
+      'import/no-unresolved': 'off',
       'jsdoc/check-param-names': ['error', { checkDestructured: false }],
       'jsdoc/require-hyphen-before-param-description': 'error',
       'jsdoc/require-jsdoc': ['error', { require: { FunctionExpression: true, ArrowFunctionExpression: true } }],
@@ -44,9 +34,8 @@ export default [
       '@typescript-eslint/non-nullable-type-assertion-style': 'off',
       '@typescript-eslint/promise-function-async': 'off',
       '@typescript-eslint/strict-boolean-expressions': ['error', { allowNullableObject: true }],
-      '@typescript-eslint/space-before-function-paren': 'off'
-      // this rule should be enabled when eslint-config-love updates peer dependencies
-      // 'n/prefer-node-protocol': 'error'
+      '@typescript-eslint/space-before-function-paren': 'off',
+      'n/prefer-node-protocol': 'error'
     }
   },
   // vitest
