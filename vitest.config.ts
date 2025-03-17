5a07ce28463c3fb501affe3f5709a8ede334bed9
Jason Hyde <github@2bad.me>
2025-03-17 04:49:16 +0100

    [misc] add linting rule ignore comment

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { coverageConfigDefaults, defineConfig } from 'vitest/config'
 
+// eslint-disable-next-line import-x/no-default-export
 export default defineConfig({
   test: {
     exclude: ['build', 'node_modules'],
