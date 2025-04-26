49bf841174c49314a15d239d11864ed1f5cd4e76
Jason Hyde <github@2bad.me>
2025-04-27 00:11:02 +0200

    [misc] update min engine version and linter rules

--- source/decorators/index.ts
+++ source/decorators/index.ts
@@ -1,2 +1,3 @@
+// biome-ignore lint/performance/noBarrelFile:
 export { auth } from './auth.ts'
 export { measure } from './measure.ts'
--- source/lib/struct/index.ts
+++ source/lib/struct/index.ts
@@ -1,3 +1,4 @@
+// biome-ignore lint/performance/noBarrelFile:
 export { LPNET_SDK_DEVICEINFO } from './LPNET_SDK_DEVICEINFO.ts'
 export { NET_SDK_DEVICE_DISCOVERY_INFO } from './NET_SDK_DEVICE_DISCOVERY_INFO.ts'
 export { NET_SDK_IPC_DEVICE_INFO } from './NET_SDK_IPC_DEVICE_INFO.ts'
