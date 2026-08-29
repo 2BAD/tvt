5a15cdd1d201c3271b44e555dc4c65f2a3d23d1c
Jason Hyde <github@2bad.me>
2026-08-29 23:08:10 +0200

    disable no-redeclare for value/type name pairs

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,4 +1,10 @@
 import { axiom } from '@2bad/axiom'
 import { defineConfig } from 'oxlint'
 
-export default defineConfig({ extends: [axiom] })
+export default defineConfig({
+  extends: [axiom],
+  rules: {
+    // value + type declarations sharing a name (const X / type X) are valid TS, not a redeclaration
+    'no-redeclare': 'off'
+  }
+})
